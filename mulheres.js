1ba4ff074881723815fea42bfecb681b9cd046ab
a-danielabarreto <danielabna@gmail.com>
2023-05-08 12:14:40 -0300

    insere a nova versão do projeto integrado com o mongodb

--- mulheres.js
+++ mulheres.js
@@ -1,36 +1,94 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express") //aqui estou iniciando o express
+const router = express.Router() //aqui estou configurando a primeira parte da rota
+const cors = require('cors') //aqui estou trazendo o pacote cors que permite consumir essa api no front-end
+const conectaBancoDeDados = require('./bandoDeDados') //aqui estou ligando ao arquivo bancoDeDados
+conectaBancoDeDados() //aqui estou chamando a função que conecta o banco de dados
 
-const app = express()
-const porta = 3333
+const Mulher = require('./mulherModel')
 
-const mulheres = [
-    {
-        nome: 'Simaria Conceição',
-        imagem: 'https://github.com/simaraconceicao.png',
-        minibio: 'Dev e instrutora'
-    },
+const app = express() //aqui estou iniciando o app
+app.use(express.json())
+app.use(cors())
 
-    {
-        nome: 'Iana Chan',
-        imagem: 'https://media.licdn.com/dms/image/C4D03AQE-aD7nj2W_0Q/profile-displayphoto-shrink_200_200/0/1563383651406?e=1687392000&v=beta&t=7MnF1oVzWyvwEdscYvb6rph2_OoOVH4vaxKfeAKwUb4',
-        minibio: 'Fundadora da PrograMaria'
-    },
+const porta = 3333 //aqui estou criando a porta
 
-    {
-        nome: 'Daniela Barrêto',
-        imagem: 'https://media.licdn.com/dms/image/D4D35AQHKYCAmBvdEyg/profile-framedphoto-shrink_200_200/0/1664755172060?e=1682730000&v=beta&t=KvJ52xWQeN45qGsRYwcEoVFDb7CXpSFiMSlVP-TIAxU',
-        minibio: 'Estudante'
+//GET
+async function mostraMulheres(request, response) {
+    try {
+        const mulheresVindasDoBancoDeDados = await Mulher.find()
+
+        response.json(mulheresVindasDoBancoDeDados)
+
+    }catch (erro) {
+        console.log(erro)
     }
-]
 
-function mostraMulheres(request, response) {
-    response.json(mulheres)
 }
 
+//POST
+async function criaMulher(request, response) {
+    const novaMulher = new Mulher({
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio,
+        citacao: request.body.citacao
+    })
+
+    try {
+        const mulherCriada = await novaMulher.save()
+        response.status(201).json(mulherCriada)
+    } catch (erro) {
+        console.log(erro)
+    }
+}
+    
+  
+//PATCH
+async function corrigeMulher(request, response) {
+    try {
+        const mulherEncontrada = await Mulher.findById(request.params.id)
+       
+        if (request.body.nome) {
+            mulherEncontrada.nome = request.body.nome     
+        }
+    
+        if (request.body.minibio) {
+            mulherEncontrada.minibio = request.body.minibio     
+        }
+    
+        if (request.body.imagem) {
+            mulherEncontrada.imagem = request.body.imagem     
+        }
+
+        if (request.body.citacao) {
+            mulherEncontrada.citacao = request.body.citacao
+        }
+        
+        const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+        
+        response.json(mulherAtualizadaNoBancoDeDados)
+    } catch(erro) {
+        console.log(erro)
+    }       
+}
+
+//DELETE
+async function deletaMulher(request, response){
+    try {
+        await Mulher.findByIdAndDelete(request.params.id)
+        response.json({ mensagem: 'Mulher deletada com sucesso!'})
+    } catch(erro) {
+        console.log(erro)
+    }
+}
+
+//PORTA
 function mostraPorta() {
     console.log ("Servidor criado e rodando na porta ", porta)
 }
 
-app.use(router.get('/mulheres', mostraMulheres))
-app.listen(porta, mostraPorta)
+app.use(router.get('/mulheres', mostraMulheres)) //configurei rota GET /mulheres
+app.use(router.post('/mulheres', criaMulher)) //configurei rota POST /mulheres
+app.use(router.patch('/mulheres/:id', corrigeMulher)) //configurei a rota PATCH /mulheres/:id
+app.use(router.delete('/mulheres/:id', deletaMulher)) //configurei rota DELETE /mulheres
+app.listen(porta, mostraPorta) //servidor ouvindo a porta
